c792a2c507b8ac551e03ad483ffc45af110d95c2
zhongjunkai <1731812286@qq.com>
2019-04-17 16:35:30 +0800

    格式化代码

--- packages/brand-picker/src/decorator.ts
+++ packages/brand-picker/src/decorator.ts
@@ -7,24 +7,22 @@ import { Prop as config, Model as model, Watch as watch, Inject as inject, Provi
 import Vue from "vue";
 export { component, mixins, config, model, watch, inject, provide };
 
-import iview, {Notice, Message} from "iview";
+import iview, { Notice, Message } from "iview";
 const components: any = iview;
 export class Component extends Vue {
     /**
      * 获取一个全局消息提示框实例。
      * @returns Message
      */
-    protected get $message(): Message
-    {
+    protected get $message(): Message {
         return components.Message;
     }
-     
+
     /**
      * 获取一个全局通知提醒实例。
      * @returns Notice
      */
-    protected get $notice(): Notice
-    {
+    protected get $notice(): Notice {
         return components.Notice;
     }
 }
