b9cb40bd2a01d09dad2915235bbd5a88f88a67a2
zhongjunkai <1731812286@qq.com>
2019-04-17 16:03:21 +0800

    号牌组件移植

--- packages/plate-picker/src/components/test/index.ts
+++
@@ -1,9 +0,0 @@
-import { component, Component } from "../../decorator";
-import "./index.scss";
-/**
- * @description 特色服务与组织机构组件
- */
-@component({ template: require("./index.html"), components: {} })
-export default class TestComponent extends Component {
-
-}
