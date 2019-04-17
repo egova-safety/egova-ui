40b56a8571dac7ba7eaaf161b5cb9af3343e1697
songjh <799724471@qq.com>
2019-04-17 16:42:31 +0800

    [M]upload组件修改Component类

--- packages/image-upload/src/decorator.ts
+++ packages/image-upload/src/decorator.ts
@@ -6,16 +6,15 @@ import component, { mixins } from "vue-class-component";
 import { Prop as config, Model as model, Watch as watch, Inject as inject, Provide as provide } from "vue-property-decorator";
 import Vue from "vue";
 export { component, mixins, config, model, watch, inject, provide };
-import {  Message, Notice } from "iview";
+import iview, {  Message, Notice } from "iview";
+const components: any = iview;
 export class Component extends Vue {
-    /**
-     * 获取一个全局通知提醒实例。
-     * @returns Notice
-     */
-    protected readonly $notice: Notice;
-    /**
-     * 获取一个全局消息提示框实例。
-     * @returns Message
-     */
-    protected readonly $message: Message;
+    protected get $message(): Message
+    {
+        return components.Message;
+    }
+    protected get $notice(): Notice
+    {
+        return components.Notice;
+    }
 }
