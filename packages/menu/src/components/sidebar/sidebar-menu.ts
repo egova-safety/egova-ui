7b9f663ea18b5b4c5611ccb4a0f24a6de4dc0392
zhongjunkai <1731812286@qq.com>
2019-05-07 17:47:58 +0800

    菜单选中判断逻辑修改

--- packages/menu/src/components/sidebar/sidebar-menu.ts
+++ packages/menu/src/components/sidebar/sidebar-menu.ts
@@ -49,18 +49,29 @@ export default class SidebarMenu extends Component {
     public update(): void {
         this.$nextTick(() => {
             if (this.$refs.sideMenu) {
-                // this.$refs["sideMenu"].updateOpened();
-                // (<any>this.$refs.sideMenu).updateActiveName();
-                // this.$refs.sideMenu["activeName"] = (<any>this).$route.name;
-                this.active = (<any>this).$route.name;
-                this.$forceUpdate();
+                // this.active = (<any>this).$route.name;
+                // this.$forceUpdate();
+
+                this.setActiveName(this.menuList, (<any>this).$route.name);
             }
         });
     }
 
+    public setActiveName(menuList: Array<any>, name: string) {
+        for (let menu of menuList) {
+            if (menu.name === name) this.active = name;
+            else if (name.indexOf(menu.name) >= 0 && menu.name !== name) {
+                if (menu.children && menu.children.length) {
+                    this.setActiveName(menu.children, name);
+                } else this.active = menu.name;
+            }
+        }
+        this.$forceUpdate();
+    }
+
     public mounted() {
         let that = this;
-        let interval = setTimeout(function() {
+        let interval = setTimeout(function () {
             if (!that.isOpened) {
                 that.updateOpenMenu(that);
             } else {
